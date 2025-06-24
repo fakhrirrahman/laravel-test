a9f0132e4b1264f145673aa89eada6ecf91dd555
fahrir <fakhrirrahman7@gmail.com>
2025-06-24 13:40:09 +0700

    fix: ensure password is hashed when seeding test user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,7 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password'),
         ]);
 
         User::factory(10)->create();
