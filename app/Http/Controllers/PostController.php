0558029184a5d5fe9ab2e5e76114e72e67c8f8ee
fahrir <fakhrirrahman7@gmail.com>
2025-06-24 13:32:20 +0700

    add PostController with index method to retrieve published posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('user')
+            ->where('is_draft', false)
+            ->whereNotNull('published_at')
+            ->where('published_at', '<=', now())
+            ->paginate(20);
+
+        return response()->json($posts);
+    }
+}
