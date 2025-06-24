2fb38ec6a3f15cc4252de9bdd0d848b602131435
fahrir <fakhrirrahman7@gmail.com>
2025-06-24 13:39:41 +0700

    add create method to PostController for post creation endpoint

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,9 @@ class PostController extends Controller
 
         return response()->json($posts);
     }
+
+    public function create()
+    {
+        return response()->json('posts.create');
+    }
 }
